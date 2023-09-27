dcd835e0bade6cf67562bc44fe3a3a109353e20c
Sahan Hyanju <hyanjusahan@gmail.com>
2023-09-27 22:33:40 +0545

    talkie

--- talkie/src/App.js
+++ talkie/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./index.css";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import NavbarComponent from "./component/NavbarComponent";
+import HomeComponent from "./component/HomeComponent";
+import RegisterComponent from "./component/RegisterComponent";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<RegisterComponent/>}/>
+        <Route path="/home" element={<HomeComponent/>}/>
+      </Routes>
+    </Router>
   );
 }
 
