92fa3d8db45bc9910554747e2d75576219480fab
sahan <hyanjusahan@gmail.com>
2023-09-28 12:14:20 +0545

    register

--- ecommerce/src/component/frontend/RegisterComponent.js
+++ ecommerce/src/component/frontend/RegisterComponent.js
@@ -1,20 +1,59 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 function RegisterComponent() {
-    const {email,setEmail} = useState('');
-    const {password,setPassword} = useState('');
-    return (
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const data = {
+      email: email,
+      password: password,
+    };
+    axios
+      .post("http://localhost:5000/users/register", data)
+      .then((response) => {
+        console.log(response);
+        alert(`success:${response.data.msg}`);
+
+        // setTimeout(()=>{
+        //  window.location.href="/login";
+        // },1000);
+
+      })
+      .catch((err) => {
+        // alert(`error:${err.response.data.msg}`);
+        console.log(err.response.data.msg);
+      });
+  };
+  return (
     <>
       <form>
         <div className="form-group">
-            <label>Email</label>
-            <input type="email" id="inputEmail" onChange={(e)=>setEmail(e.target.value)}/>
+          <label>Email</label>
+          {/* <p>{email}</p> */}
+          <input
+            type="email"
+            id="inputEmail"
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
         </div>
         <div className="form-group">
-            <label>Password</label>
-            <input type="password" id="inputPassword" onChange={(e)=>setPassword(e.target.value)}/>
+          <label>Password</label>
+          <input
+            type="password"
+            id="inputPassword"
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
         </div>
-        <button className="btn btn-primary" type="submit">Submit</button>
+        <button
+          className="btn btn-primary"
+          onClick={handleSubmit}
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </>
   );
